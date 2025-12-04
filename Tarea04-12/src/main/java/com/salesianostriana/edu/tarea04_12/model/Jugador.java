26f0b3511e30df66dc2a63f2bb98a297a2293e16
PedritoSerrano <serrano.roped24@triana.salesianos.edu>
2025-12-04 20:51:24 +0100

    acabado

--- Tarea04-12/src/main/java/com/salesianostriana/edu/tarea04_12/model/Jugador.java
+++ Tarea04-12/src/main/java/com/salesianostriana/edu/tarea04_12/model/Jugador.java
@@ -3,14 +3,22 @@ package com.salesianostriana.edu.tarea04_12.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Objects;
+
 @Entity
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
+@Table(name = "jugador")
 public class Jugador {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "player_generator")
+    @SequenceGenerator(name = "player_generator", sequenceName = "player_seq", allocationSize = 1)
     private Long id;
 
     private String nickname;
@@ -18,6 +26,18 @@ public class Jugador {
     private int edad;
 
     @ManyToOne
-    @JoinColumn(name = "equipo_id")
+    @JoinColumn(foreignKey =
+        @ForeignKey(name = "fk_jugador_equipo"))
     private Equipo equipo;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Jugador jugador)) return false;
+        return edad == jugador.edad && Objects.equals(id, jugador.id) && Objects.equals(nickname, jugador.nickname) && Objects.equals(nombreReal, jugador.nombreReal) && Objects.equals(equipo, jugador.equipo);
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(id, nickname, nombreReal, edad, equipo);
+    }
 }
